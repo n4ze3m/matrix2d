ce4e783df1c2c55c065590b1eb15e619834cf302
nazeem <nazeemnob18@gmail.com>
2022-09-14 16:40:59 +0530

    slice type issue fixed

--- lib/matrix2d.dart
+++ lib/matrix2d.dart
@@ -2,6 +2,6 @@ library matrix2d;
 
 /// Dart package for 2d matrix or list operations
 /// The library was developed, documented, and published by
-/// [Jose Gracia Mccausland](https://twitter.com/juveaj) and [Muhammed Nazeem](https://twitter.com/JuventusRuling)
+/// [Jose Gracia Mccausland](https://twitter.com/juveaj) and [Muhammed Nazeem](https://twitter.com/n4ze3m)
 export 'src/matrix2d.dart';
 export 'src/extension/extension.dart';
